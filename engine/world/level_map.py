4b89e7e2b29adc8103f58742aec9d149813654a6
Arch4ngel21 <92851969+Arch4ngel21@users.noreply.github.com>
2022-03-24 14:19:18 +0100

    zmiany do testów

--- engine/world/level_map.py
+++ engine/world/level_map.py
@@ -28,8 +28,58 @@ class LevelMap:
 
         for y in range(self.level_map_height):
             for x in range(self.level_map_width):
-                px = image.getpixel((x, y))
+                r, g, b = image.getpixel((x, y))
 
+                if (26, 213, 0) == (r, g, b):
+
+                    rand = random.randint(0, 7)
+                    if rand == 0:
+                        level[y][x] = Block(x, y, "grass4", True)
+                    elif rand == 1:
+                        level[y][x] = Block(x, y, "grass1", True)
+                    elif rand in [2, 3, 4, 5, 6]:
+                        level[y][x] = Block(x, y, "grass2", True)
+                    else:
+                        level[y][x] = Block(x, y, "grass3", True)
+
+                elif (18, 146, 0) == (r, g, b):
+                    level[y][x] = Block(x, y, "bush", False)
+
+                elif (146, 146, 146) == (r, g, b):
+                    rand = random.randint(0, 4)
+                    if rand == 0:
+                        level[y][x] = Block(x, y, "bricks1", True)
+                    elif rand == 1:
+                        level[y][x] = Block(x, y, "bricks2", True)
+                    elif rand == 2:
+                        level[y][x] = Block(x, y, "bricks3", True)
+                    elif rand == 3:
+                        level[y][x] = Block(x, y, "bricks4", True)
+
+                elif (85, 85, 85) == (r, g, b):
+
+                    if (self.world_map_x == 4 and self.world_map_y == 1) or (self.world_map_x == 4 and self.world_map_y == 3) \
+                            or (self.world_map_x == 2 and self.world_map_y == 3):
+                        level[y][x] = Block(x, y, "cobblestone", False)
+                    else:
+                        level[y][x] = Block(x, y, "cobblestone", True)
+
+                elif (0, 83, 197) == (r, g, b):
+                    level[y][x] = (x, y, "water", False)
+
+                elif (150, 106, 0) == (r, g, b):
+                    # TODO -> obracanie ścieżki
+                    pass
+
+                elif (195, 193, 0) == (r, g, b):
+                    # TODO -> obracanie brzegu
+                    pass
+
+                elif (116, 87, 0) == (r, g, b):
+                    level[y][x] = Block(x, y, "tree_bottom", False)
+
+                elif (8, 64, 0) == (r, g, b):
+                    level[y][x] = Block(x, y, "tree_up", False)
 
 
 
