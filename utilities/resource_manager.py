657db7463cd6ff0ddd57b3c6c4e6873479200e17
Arch4ngel21 <92851969+Arch4ngel21@users.noreply.github.com>
2022-03-24 13:38:31 +0100

    lol 4 commit

--- utilities/resource_manager.py
+++ utilities/resource_manager.py
@@ -1,6 +1,8 @@
 
 from PIL import Image
 
+level_4_4 = Image.open(r"../resources/maps/3_0.png")
+
 
 class ResourceManager:
     level_2_0 = Image.open(r"../resources/maps/2_0.png")
@@ -8,16 +10,16 @@ class ResourceManager:
     level_2_1 = Image.open(r"../resources/maps/2_1.png")
     level_3_1 = Image.open(r"../resources/maps/3_1.png")
     level_4_1 = Image.open(r"../resources/maps/4_1.png")
-    level_0_2 = Image.open(r"../resources/maps/3_0.png")
-    level_1_2 = Image.open(r"../resources/maps/3_0.png")
-    level_4_2 = Image.open(r"../resources/maps/3_0.png")
-    level_0_3 = Image.open(r"../resources/maps/3_0.png")
-    level_1_3 = Image.open(r"../resources/maps/3_0.png")
-    level_2_3 = Image.open(r"../resources/maps/3_0.png")
-    level_3_3 = Image.open(r"../resources/maps/3_0.png")
-    level_4_3 = Image.open(r"../resources/maps/3_0.png")
-    level_0_4 = Image.open(r"../resources/maps/3_0.png")
-    level_1_4 = Image.open(r"../resources/maps/3_0.png")
+    level_0_2 = Image.open(r"../resources/maps/0_2.png")
+    level_1_2 = Image.open(r"../resources/maps/1_2.png")
+    level_4_2 = Image.open(r"../resources/maps/4_2.png")
+    level_0_3 = Image.open(r"../resources/maps/0_3.png")
+    level_1_3 = Image.open(r"../resources/maps/1_3.png")
+    level_2_3 = Image.open(r"../resources/maps/2_3.png")
+    level_3_3 = Image.open(r"../resources/maps/3_3.png")
+    level_4_3 = Image.open(r"../resources/maps/4_3.png")
+    level_0_4 = Image.open(r"../resources/maps/0_4.png")
+    level_1_4 = Image.open(r"../resources/maps/1_4.png")
     level_2_4 = Image.open(r"../resources/maps/3_0.png")
     level_3_4 = Image.open(r"../resources/maps/3_0.png")
     level_4_4 = Image.open(r"../resources/maps/3_0.png")
