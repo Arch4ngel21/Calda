cb0a2b20a690966d50547c9a87afd4100ded5cf1
bblachut1 <bogojedi1@gmail.com>
2022-03-25 21:08:00 +0100

    add peaceful_entity fields and methods

--- engine/entities/peaceful_entity.py
+++ engine/entities/peaceful_entity.py
@@ -1,2 +1,29 @@
-class PeacefulEntity:
-    pass
+from enum import Enum
+import pygame
+
+from engine.entities.entity import Entity
+
+
+class PeacefulEntityType(Enum):
+    SIGN = 1
+    DUNGEON_ENTRANCE = 2
+    TRE_OF_HEALTH = 3
+
+
+class PeacefulEntity(Entity):
+    def __init__(self, x: int, y: int, health: int, damage: int, peaceful_entity_type: PeacefulEntityType):
+        super().__init__(x, y, health, damage)
+        self._passive_effect_frame: int = 0
+        self._peaceful_entity_type = peaceful_entity_type
+        self._bounding_box = pygame.Rect(x, y, 32, 32)
+
+    def increase_animation_frame(self):
+        self._animation_frame += 1
+        if self._animation_frame >= 40:
+            self._animation_frame = 0
+
+    def increase_passive_effect_frame(self):
+        self._passive_effect_frame += 1
+        if self._passive_effect_frame >= 200:
+            self._passive_effect_frame = 0
+
