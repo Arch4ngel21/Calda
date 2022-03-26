2938352b647d2a150d626e7353782035d7743cf2
bblachut1 <bogojedi1@gmail.com>
2022-03-26 13:37:45 +0100

    add screen_effect fields and methods

--- engine/entities/effects/screen_effect.py
+++ engine/entities/effects/screen_effect.py
@@ -1,4 +1,32 @@
 
 class ScreenEffect:
-    def __init__(self):
-        pass
+    def __init__(self, x: int, y: int, duration: int):
+        self._x: int = x
+        self._y: int = y
+        self._duration: int = duration
+        self._animation_frame = 0
+
+    def increase_animation_frame(self):
+        if self._animation_frame >= self._duration:
+            return
+        self._animation_frame += 1
+
+    @property
+    def x(self) -> int:
+        return self._x
+
+    @x.setter
+    def x(self, value: int):
+        if not isinstance(value, int):
+            raise ValueError("x must be an int")
+        self._x = value
+
+    @property
+    def y(self) -> int:
+        return self._y
+
+    @y.setter
+    def y(self, value: int):
+        if not isinstance(value, int):
+            raise ValueError("y must be an int")
+        self._y = value
