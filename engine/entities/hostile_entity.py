7f421be83894847934ab060c53361b489e0e5c7c
bblachut1 <bogojedi1@gmail.com>
2022-04-21 20:04:39 +0200

    add move()

--- engine/entities/hostile_entity.py
+++ engine/entities/hostile_entity.py
@@ -12,20 +12,38 @@ class HostileEntityType(Enum):
 
 
 class HostileEntity(Entity):
+    BOX_SIZE: int = 32
     def __init__(self, x: int, y: int, health: int, damage: int, entity_type: HostileEntityType):
         super().__init__(x, y, health, damage)
         self._steps = 0
         self._attack_frame: int = 0
         self._is_attacking: bool = False
         self._entity_type: HostileEntityType = entity_type
-        self._hit_box: pygame.Rect = pygame.Rect(x, y, 32, 32)
-        self._bounding_box: pygame.Rect = pygame.Rect(x, y, 32, 32)
+        self._hit_box: pygame.Rect = pygame.Rect(x, y, self.BOX_SIZE, self.BOX_SIZE)
+        self._bounding_box: pygame.Rect = pygame.Rect(x, y, self.BOX_SIZE, self.BOX_SIZE)
         self._facing: MapDirection = MapDirection.WEST
 
     def move(self):
-        # TODO
-        pass
+        if self._steps % 3 == 0:
+            if self._facing == MapDirection.NORTH:
+                self._y -= 1
+            if self._facing == MapDirection.WEST:
+                self._x -= 1
+            if self._facing == MapDirection.EAST:
+                self._x += 1
+            if self._facing == MapDirection.SOUTH:
+                self._y += 1
+            self._bounding_box.update(self._x, self._x, self.BOX_SIZE, self.BOX_SIZE)
+            self._hit_box.update(self._x, self._x, self.BOX_SIZE, self.BOX_SIZE)
 
+        self.increase_steps()
+        if self._steps == 0:
+            self._facing = self.new_direction()
+
+        self.increase_steps()
+
+    #TODO
+    # zastanowić się czy te metody mają być publiczne
     def increase_animation_frame(self):
         self._animation_frame += 1
         if self._animation_frame >= 500:
--- engine/entities/player.py
+++ engine/entities/player.py
@@ -4,18 +4,35 @@ from engine.entities.entity import Entity
 
 
 class Player(Entity):
+    BOUNDING_BOX_SIZE: int = 48
+    HIT_BOX_SIZE: int = 32
 
     def __init__(self, x: int, y: int, health: int, damage: int):
         super().__init__(x, y, health, damage)
         self._coins: int = 0
         self._has_sword: bool = False
         self._facing: MapDirection = MapDirection.WEST
-        self._hit_box: pygame.Rect = pygame.Rect(x, y, 32, 32)
+        self._hit_box: pygame.Rect = pygame.Rect(x, y, self.HIT_BOX_SIZE, self.HIT_BOX_SIZE)
+        self._bounding_box: pygame.Rect = pygame.Rect(x, y, self.BOUNDING_BOX_SIZE, self.HIT_BOX_SIZE)
         self._attack_frame = 0
 
     def move(self):
-        # TODO
-        pass
+        if self._facing == MapDirection.NORTH:
+            self._y -= 1
+            self._hit_box.update(self._x, self._x, self.HIT_BOX_SIZE, self.BOUNDING_BOX_SIZE)
+        if self._facing == MapDirection.WEST:
+            self._x -= 1
+            self._hit_box.update(self._x, self._x, self.BOUNDING_BOX_SIZE, self.HIT_BOX_SIZE)
+        if self._facing == MapDirection.EAST:
+            self._x += 1
+            self._hit_box.update(self._x, self._x, self.BOUNDING_BOX_SIZE, self.HIT_BOX_SIZE)
+        if self._facing == MapDirection.SOUTH:
+            self._y += 1
+            self._hit_box.update(self._x, self._x, self.HIT_BOX_SIZE, self.BOUNDING_BOX_SIZE)
+
+        self._bounding_box.update(self._x, self._x, self.BOUNDING_BOX_SIZE, self.BOUNDING_BOX_SIZE)
+
+        self.increase_animation_frame()
 
     def add_health(self):
         self.heal(2)
@@ -31,6 +48,11 @@ class Player(Entity):
         self._attack_frame += 1
         if self._attack_frame > 30:
             self._attack_frame = 0
+
+    def increase_animation_frame(self):
+        self._animation_frame += 1
+        if self._animation_frame == 40:
+            self._animation_frame = 0
 
     @property
     def has_sword(self) -> bool:
